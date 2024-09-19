4f4eadb3e44e6bd4c84810ddaeafc2f34c79e6cd
MRizwanRohan <rizwanuddin1100@gmail.com>
2024-09-19 23:10:35 +0600

    first commit

---
+++ src/const/const.js
@@ -0,0 +1,225 @@
+export const studies = [
+  {
+    title: "Técnico en desarrollo de software",
+    corporation: "SENA - Montería (Córdoba) ",
+    date: "02/2019 – 12/2019",
+  },
+  {
+    title: "Desarrollador Front-end React Js",
+    corporation: "Alura Latam - Remoto",
+    date: "04/2023 – 09/2023",
+  },
+  {
+    title: "Curso profesional de React Js",
+    corporation: "Código facilito - Remoto",
+    date: "04/2023 – 04/2023",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+
+];
+
+//edit pic and styles
+export const projects = [
+  {
+    img: "../../../img/tasks-manager.webp",
+    title: "Tasks Manager",
+    link: "https://task-manager-ecru-chi.vercel.app/dashboard",
+    description: `Administrador de tareas con drag and drop autenticación con Clerk , shadcn.  "En Desarrollo "`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.zustand,
+      },
+      {
+        icon: IconKeys.shadcn,
+      },
+    ],
+  },
+  {
+    img: "../../../img/ecommerce-stripe-2.webp",
+    title: "Ecommerce Stripe",
+    link: "https://ecommerce-stripe-763m.vercel.app/",
+    description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.mongodb,
+      },
+      {
+        icon: IconKeys.stripe,
+      },
+    ],
+  },
+  {
+    img: "../../../img/nexanime.webp",
+    title: "NexAnime",
+    link: "https://nexanime.vercel.app",
+    description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+  {
+    img: "../../../img/store-games.webp",
+    title: "Store games",
+    link: "https://store-games-site.netlify.app/",
+    description: `Simulando una página de juegos , consumiendo la API de rawg api
+      donde los usuarios pueden buscar videojuegos, ver sus detalles como
+      creadores , calificaciones,tiendas donde comprar.....`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.reactQuery,
+      },
+    ],
+  },
+  {
+    img: "../../../img/movie.webp",
+    title: "Movie Tv",
+    link: "https://movietvsite.netlify.app/",
+    description: `simulando una página de películas , consumiendo la API de themovietv
+              donde los usuarios pueden buscar películas y ver trailer.....`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+  {
+    img: "../../../img/cyberpunk.webp",
+    title: "cyberpunk",
+    link: "https://preeminent-liger-266356.netlify.app/",
+    description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
+      el ususario puede ver los tailer de él juegos y también los
+      episodios de la serie....`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+    ],
+  },
+
+  
+
+  
+];
